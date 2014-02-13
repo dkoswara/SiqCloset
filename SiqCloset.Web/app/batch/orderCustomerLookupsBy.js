d083dedfb40856e0826f48e2ad6bb7759581dda7
dkoswara <denisk@ideablade.com>
2014-02-13 00:33:12 -0800

    Issue #10 - BatchDetail - Customer lookups typeahead need to show filtered list with better relevance

---
+++ SiqCloset.Web/app/batch/orderCustomerLookupsBy.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('app').filter('orderCustomerLookupsBy', function() {
+      return function(customers, viewValue) {
+        var filtered = [];
+        
+        angular.forEach(customers, function(customer) {
+          customer.relevance = getRelevance(customer.name, viewValue);
+          filtered.push(customer);
+        });
+        
+        return filtered;
+
+        function getRelevance(customerName, searchValue) {
+          if(customerName.substr(0, searchValue.length).toLowerCase() == searchValue.toLowerCase()) {
+            return 1;
+          }
+
+          var idx = customerName.indexOf(" ") + 1;
+          var lastName = customerName.substr(idx);
+          if(lastName.substr(0, searchValue.length).toLowerCase() == searchValue.toLowerCase()) {
+            return 2;
+          }
+
+          if(customerName.lastIndexOf(searchValue) != -1) {
+            return 3;
+          }
+          return 4;
+        }
+      };
+    })
+
+})();
