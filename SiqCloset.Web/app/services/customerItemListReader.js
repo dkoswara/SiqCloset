33b76bd42bc4a5ee067c88fa135d653cbaa4fb14
dkoswara <denisk@ideablade.com>
2014-04-29 11:58:44 -0700

    Added excel tests. Modify customerItemListReader to be more robust.

--- SiqCloset.Web/app/services/customerItemListReader.js
+++ SiqCloset.Web/app/services/customerItemListReader.js
@@ -6,6 +6,8 @@
     angular.module('app').factory(serviceId, [customerItemListReader]);
 
     function customerItemListReader() {
+        var underscore = _;
+
         var service = {
             getCustItemList: getCustItemList,
         };
@@ -14,20 +16,78 @@
 
         function getCustItemList(data, batchNo) {
             var workbook = convertRawDataToWorkbook(data);
-            return convertSheetToJson(workbook, batchNo);
+            return convertSheetToObject(workbook, batchNo);
         }
 
         function convertRawDataToWorkbook(data) {
-            var cfb = XLS.CFB.read(data, { type: "binary" });
+            var cfb = XLS.CFB.read(data, { type: 'binary' });
             var v = XLS.parse_xlscfb(cfb);
             return v;
         }
 
-        function convertSheetToJson(workbook, batchNumber) {
+        function convertSheetToObject(workbook, batchNumber) {
             var sheetName = 'BATCH ' + batchNumber;
             var sheet = workbook.Sheets[sheetName];
-            var json = XLS.utils.sheet_to_row_object_array(sheet);
-            return json;
+            return createCustItemListObject(sheet);
+        }
+
+        function getFirstAndLastRowIndex(sheet) {
+            //get the keys
+            var keys = Object.keys(sheet);
+
+            //get the B's and max number
+            //the B column is where the Customer name is
+            //and they're usually always populated
+            var allTheBs = underscore.filter(keys, function(x) {
+                return x.substr(0, 1) == 'B';
+            });
+
+            var min = underscore.min(allTheBs, function (x) {
+                var num = x.substr(1);
+                return parseInt(num);
+            });
+
+            var max = underscore.max(allTheBs, function (x) {
+                var num = x.substr(1);
+                return parseInt(num);
+            });
+
+            return {
+                firstRowIndex: parseInt(min.substr(1)),
+                lastRowIndex: parseInt(max.substr(1)),
+            };
+        }
+
+        function createCustItemListObject(sheet) {
+            var x = getFirstAndLastRowIndex(sheet);
+            var custItemList = [];
+            for (var i = x.firstRowIndex; i <= x.lastRowIndex; i++) {
+                var custName = getCellValue(sheet, 'B', i);
+                if (!custName) continue;
+                var notes = getCellValue(sheet, 'A', i);
+                var itemCode = getCellValue(sheet, 'C', i);
+                var itemName = getCellValue(sheet, 'D', i);
+                var boxNo = getCellValue(sheet, 'E', i, 0);
+                var itemPrice = getCellValue(sheet, 'F', i, 0);
+
+                var cil = {
+                    Notes: notes,
+                    CustomerName: custName,
+                    ItemCode: itemCode,
+                    ItemName: itemName,
+                    BoxNo: boxNo,
+                    Price: itemPrice,
+                }
+                custItemList.push(cil);
+            }
+
+            return custItemList;
+
+            function getCellValue(s, letter, row, defaultValue) {
+                defaultValue = defaultValue || '';
+                var cellPos = letter + '' + row;
+                return s[cellPos] ? JSON.parse(s[cellPos].v) : defaultValue;
+            }
         }
     }
 
---
+++ SiqCloset.Web/tests/excelTests.js
@@ -0,0 +1,105 @@
+﻿jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000;
+
+describe("Excel related tests", function() {
+
+    var customerItemListReader;
+    var filename = 'http://localhost:3958/tests/CustomerItemList.xls';
+    var $angularQ;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function($injector, $q) {
+        customerItemListReader = $injector.get('customerItemListReader');
+        $angularQ = $q;
+    }));
+
+    it('should read CIL', function(done) {
+        downloadExcelFile(filename)
+            .then(function(data) {
+                var results = customerItemListReader.getCustItemList(data, '136');
+                done();
+            }).fail(function(error) {
+                console.log(error);
+                done();
+            });
+    });
+
+    function downloadExcelFile(excelFilename) {
+        var ajaxRequest = {
+            url: excelFilename,
+            beforeSend: function (xhr) {
+                xhr.overrideMimeType('text/plain; charset=x-user-defined');
+            },
+        };
+        return $.ajax(ajaxRequest)
+            .done(function (data) {
+                return data;
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                return errorThrown;
+            });
+    }
+
+    //function load_binary_resource(url) {
+    //    var req = new XMLHttpRequest();
+    //    req.open('GET', url, false);
+    //    //XHR binary charset opt by Marcus Granado 2006 [http://mgran.blogspot.com]
+    //    req.overrideMimeType('text\/plain; charset=x-user-defined');
+    //    req.send(null);
+    //    if (req.status != 200) return '';
+    //    return req.responseText;
+    //}
+
+    
+
+    //function readTextFile(file) {
+    //    var defer = $angularQ.defer();
+    //    var rawFile = new XMLHttpRequest();
+    //    rawFile.open("GET", file, true);
+    //    rawFile.onreadystatechange = function () {
+    //        //defer.resolve(rawFile.responseText);
+    //        if (rawFile.readyState === 4) {
+    //            if (rawFile.status === 200 || rawFile.status == 0) {
+    //                defer.resolve(rawFile.responseText);
+    //            }
+    //        }
+    //    }
+    //    rawFile.send(null);
+    //    return defer.promise;
+    //}
+
+    //it('should read local file using jQuery $.ajax', function (jasmineDone) {
+    //    var ajaxRequest = {
+    //        url: filename,
+    //        beforeSend: function (xhr) {
+    //            xhr.overrideMimeType('text/plain; charset=UTF-8');
+    //        },
+    //    };
+    //    $.ajax(ajaxRequest)
+    //        .done(function (data) {
+    //            expect(data).toBeTruthy();
+    //            jasmineDone();
+    //        }).fail(function (jqXHR, textStatus, errorThrown) {
+    //            console.log(jqXHR.responseText);
+    //            expect(true).toBe(false);
+    //            jasmineDone();
+    //        });
+    //});
+
+    //it('should read local file using raw XHR', function (jasmineDone) {
+    //    readTextFile(filename);
+    //    function readTextFile(file) {
+    //        var rawFile = new XMLHttpRequest();
+    //        rawFile.open("GET", file, true);
+    //        rawFile.onreadystatechange = function () {
+    //            if (rawFile.readyState === 4) {
+    //                if (rawFile.status === 200 || rawFile.status == 0) {
+    //                    var allText = rawFile.responseText;
+    //                    alert(allText);
+    //                }
+    //            }
+    //        }
+    //        rawFile.send(null);
+    //    }
+    //});
+    
+});
