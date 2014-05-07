a2b1c60982f634890260ee753cdb4715560e35b7
dkoswara <denisk@ideablade.com>
2014-05-07 15:10:30 -0700

    Dashboard specs work

--- SiqCloset.Web/tests/dashboard.specs.js
+++ SiqCloset.Web/tests/dashboard.specs.js
@@ -1,29 +1,23 @@
 ﻿ddescribe('The Dashboard', function () {
 
-	var $scope, ctrl, $timeout;
-
-	/* declare our mocks out here
-	 * so we can use them through the scope 
-	 * of this describe block.
-	 */
+	var ctrl, $timeout;
 	var commonMock;
 	var datacontextMock;
 	var currencyExchangeMock;
 
-	// This function will be called before every "it" block.
-	// This should be used to "reset" state for your tests.
 	beforeEach(function () {
-		// Create a "spy object" for each of the custom service used by dashboard.
-		// This will isolate the controller we're testing from
-		// any other code.
-	    // we'll set up the returns for this later 
-
-	    //commonMock = jasmine.createSpyObj('common', ['activateController']);	    
-	    //datacontextMock = jasmine.createSpy('datacontext');
-		//currencyExchangeMock = jasmine.createSpy('currencyExchange');
 
 		// load the module you're testing.
 		module('app');
+
+	    //by loading the module app above and injecting services below
+	    //$routeProvider will be called which does a bunch of $http calls
+	    //I'm using $httpBackend to mock the GET responses here
+        //TODO: Figure out how to mock the $routeProvider so it doesn't do anything
+		inject(function($httpBackend) {
+			$httpBackend.when('GET', 'breeze/Breeze/Metadata').respond(200);
+			$httpBackend.when('GET', 'app/dashboard/dashboard.html').respond(200);
+		});	
 
 		// INJECT! This part is critical
 		// $rootScope - injected to create a new $scope instance.
@@ -31,44 +25,49 @@
 		// $q - injected so we can create promises for our mocks.
 		// _$timeout_ - injected to we can flush unresolved promises.
 		inject(function ($rootScope, $controller, $q, _$timeout_) {
-			// create a scope object for us to use.
-			$scope = $rootScope.$new();
 
-			// set up the returns for our 'common' service mock
-			//commonMock.activateController.and.returnValue(true);
-
-			// assign $timeout to a scoped variable so we can use 
+		    // assign $timeout to a scoped variable so we can use
 			// $timeout.flush() later. Notice the _underscore_ trick
 			// so we can keep our names clean in the tests.
 			$timeout = _$timeout_;
 
-			commonMock = {
-			    logger: {
-			        getLogFn: function () { 
-			        	var logFn = function(val) {};
-			        	return logFn;
-			        }
-			    },
-			    activateController: function () { return $q.when('done'); }
-			};
-			spyOn(commonMock, 'activateController').and.returnValue($q.when('done'));
+		    (function setupCommonMock() {
+		        commonMock = jasmine.createSpyObj('common', ['activateController']);
+		        commonMock.activateController.and.callFake(function () {
+		            return $q.when(true);
+		        });
+		        commonMock.logger = {
+		            getLogFn: function () {
+		                var logFn = function (val) { };
+		                return logFn;
+		            }
+		        };
+		    })();
 
-		    datacontextMock = {
-		        customer: {
-		            getCount: function() { return $q.when(1); },
-		            getTopTenCustomers: function() { return $q.when({}); },
-		        },
-		        prime: function() { return $q.when({}); },
-		    };
+            (function setupCustomerRepoMock() {
+                var customerRepoMock = jasmine.createSpyObj('customer', ['getCount', 'getTopTenCustomers']);
+                customerRepoMock.getCount.and.callFake(function () { return $q.when(10); });
+                customerRepoMock.getTopTenCustomers.and.callFake(function () {
+                    var customersMap = [];
 
-		    spyOn(datacontextMock, 'prime').and.returnValue($q.when({}));
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(function (num) {
+                        customersMap.push({ name: 'customer ' + num, itemsCount: num * 10 });
+                    });
 
-		    currencyExchangeMock = {
-		        getData: function() { return $q.when({
-		        		data: { rate: 10000 }		        		
-		        	});
-		    	},
-		    };
+                    return $q.when(customersMap);
+                });
+
+                datacontextMock = {
+                    customer: customerRepoMock,
+                };
+            })();
+
+		    (function setupCurrencyExchangeMock() {
+		        currencyExchangeMock = jasmine.createSpyObj('currencyExchange', ['getData']);
+		        currencyExchangeMock.getData.and.callFake(function () {
+		            return $q.when({ data: { rate: 10000 } });
+		        });
+		    })();
 
 			// now run that scope through the controller function,
 			// injecting any services or other injectables we need.
@@ -89,19 +88,40 @@
 	    expect(commonMock.activateController).toHaveBeenCalled();
 	});
 
+	it('should have called datacontext.customer.getCount', function () {
+	    expect(datacontextMock.customer.getCount).toHaveBeenCalled();
+	});
+
+	it('should have called datacontext.customer.getTopTenCustomers', function () {
+	    expect(datacontextMock.customer.getTopTenCustomers).toHaveBeenCalled();
+	});
+
+	it('should have called currencyExchange.getData', function () {
+	    expect(currencyExchangeMock.getData).toHaveBeenCalled();
+	});
+
 	it('should have introduction text', function() {
 	    expect(ctrl.intro.title).toBeTruthy();
 	});
 	
-	it('should have current date and time', function () { });
+	it('should have current date and time', function() {
+	    var currDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+	    expect(ctrl.currentDateTime).toEqual(currDateTime);
+	});
 
-	it('should have customers count', function () { });
+	it('should have customers count', function() {
+	    $timeout.flush();
+	    expect(ctrl.customersCount).toBe(10);
+	});
 
-    iit('should have current currency exchange rate', function() {
+    it('should have current currency exchange rate', function() {
     	$timeout.flush();
-        expect(ctrl.exchangeRateText).toBeTruthy();
+        expect(ctrl.exchangeRateText).toContain('10000');
     });
 
-	it('should have the top ten customers', function () { });
+	it('should have the top ten customers', function() {
+	    $timeout.flush();
+	    expect(ctrl.customerSummary.summaries.length).toBe(10);
+	});
 
 });
