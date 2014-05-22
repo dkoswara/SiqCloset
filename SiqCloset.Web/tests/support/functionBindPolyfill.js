4710880fad0400b037222381b02815b1da3d46e8
dkoswara <denisk@ideablade.com>
2014-05-22 16:30:34 -0700

    Tests - Chutzpah settings file and polyfill for Function.prototype.bind to allow testing with Chutzpah and its PhantomJS runner

---
+++ SiqCloset.Web/tests/support/functionBindPolyfill.js
@@ -0,0 +1,29 @@
+﻿/*
+ * Polyfill for Function.prototype.bind.
+ * PhantomJS doesn't support Function.prototype.bind until 2.0. We're currently on 1.9.
+ * See https://github.com/ariya/phantomjs/issues/10522
+ */
+
+(function () {
+    'use strict';
+    if (typeof Function.prototype.bind !== "function") {
+        Function.prototype.bind = function (context) {
+            var slice = Array.prototype.slice;
+            var fn = this;
+
+            return function () {
+                var args = slice.call(arguments, 1);
+
+                if (args.length) {
+                    return arguments.length
+                        ? fn.apply(context, args.concat(slice.call(arguments)))
+                        : fn.apply(context, args);
+                } else {
+                    return arguments.length
+                        ? fn.apply(context, arguments)
+                        : fn.call(context);
+                }
+            };
+        };
+    }
+})();
